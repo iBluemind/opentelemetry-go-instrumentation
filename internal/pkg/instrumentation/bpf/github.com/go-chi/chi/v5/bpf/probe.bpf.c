2628efae974513b1972f0c11c141347df21e3da1
Manjong Han <manjong@payhere.in>
2024-11-25 15:17:40 +0900

    fix: fetch ctx_ptr before val_ptr fetching

--- internal/pkg/instrumentation/bpf/github.com/go-chi/chi/v5/bpf/probe.bpf.c
+++ internal/pkg/instrumentation/bpf/github.com/go-chi/chi/v5/bpf/probe.bpf.c
@@ -138,8 +138,10 @@ int uprobe_chi_Mux_routeHTTP_Returns(struct pt_regs *ctx) {
     }
 
     // get pathPattern
+    void *ctx_ptr = 0;
+    bpf_probe_read(&ctx_ptr, sizeof(ctx_ptr), (void *)(req_ptr + ctx_ptr_pos));
     void *val_ptr = 0;
-    bpf_probe_read(&val_ptr, sizeof(val_ptr), (void *)(req_ptr + val_ptr_pos));
+    bpf_probe_read(&val_ptr, sizeof(val_ptr), (void *)(ctx_ptr + val_ptr_pos));
     if (!get_go_string_from_user_ptr((void *)(val_ptr + routepattern_str_pos), http_request->path_pattern, sizeof(http_request->path_pattern))) {
         bpf_printk("failed to get path_pattern from gin context");
         return 0;
